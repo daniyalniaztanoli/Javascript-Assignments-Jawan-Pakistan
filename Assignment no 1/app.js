ce4631f2a9cf86f7523b4f52cf60b8fcdc795521
MUHAMMAD DANIYAL <127001145+daniyalniaztanoli@users.noreply.github.com>
2023-09-19 00:21:56 +0500

    Add files via upload

---
+++ Assignment no 1/app.js
@@ -0,0 +1,184 @@
+//    assignment java-script
+
+            //________________________________start________________________________// 
+
+    //--------------------------------------------------//
+                //  chapter no 1 (alerts) //
+
+    // Q : NO 1 //
+
+// var firstName = "Daniyal";
+// var lastName = "Niaz";
+// var email = "dniaz0122@gmail.com";
+// var phoneNumber = "03471257752";
+// var password = "daniyalniaz";
+
+// alert("First Name: " + firstName);
+// alert("Last Name: " + lastName);
+// alert("Email: " + email);
+// alert("Phone Number: " + phoneNumber);
+// alert("Password: " + password);
+
+    //  Q : NO 2 //
+
+// alert("you are learning javascript!");
+
+    //  Q : NO 3 //
+
+// alert("Asalam U Alikum");
+
+    //--------------------------------------------------//
+        //  cahpter no 2 (variavbles and strings) //
+
+    //  Q : NO 1//
+
+// var camelCase = "this is camel case first word is small and second word first letter is capitale";   
+
+    //  Q : NO 2//
+
+// var myVariable;
+// myVariable = "Hello world";
+
+    // Q: NO 3//
+
+// var teamName = "pakistan";
+// alert(teamName);
+    
+    //  Q: NO 4//
+    
+// var bestMan = "Daniyal"
+//  bestMan = "niaz"
+
+//--------------------------------------------------//
+     //  cahpter no 3 (variavbles and numbers) //
+
+    //  Q : NO 1//
+
+// var caseQty;
+
+    //  Q : NO 2//
+
+// caseQty = 144;
+
+    //  Q : NO 3//
+
+// var num = 9;
+
+     //  Q : NO 4//
+
+// var num1;
+// var num2;
+
+// num1 = 5;
+// num2 = 5;
+
+// var sum = (num1 + num2);
+
+// console.log(sum);
+
+    //  Q : NO 5 //
+
+// var productCost = 500;
+// var shippingCost = 100;
+// var totalCost = productcost + shippingcost ;
+
+// console.log(totalCost);
+
+ //  Q : NO 6 //
+
+// var fisrtNumber = 10 ;
+// console.log(fisrtNumber + 2);
+
+    //-------------------------------------------------------------//
+    //  cahpter no 4 (variavbles names and legal and illegal) //
+
+     //  Q : NO 1 //
+
+     
+     
+// var productcost = 3.45;   //note:  no space between letters or words //
+
+    //  Q : NO 2 //
+
+// var nameOfband ;    //note: camel case //
+
+    //  Q : NO 3 //
+
+// var firstLetter1111 = 12;
+// document.write(firstLetter1111);
+
+    //  Q : NO 4 //
+
+// var firstName = "Daniyal";
+// var lastName = "Niaz";
+
+// var combineName = firstName + (" ") + lastName ;
+
+// console.log(combineName);
+
+     //  Q : NO 5 //
+
+// legal
+
+// var nameFirst;        //note: simple small words or camel case are used
+// var name_first;       //note: this symbol are also used _ 
+// var name_first12;     //note: we used numbers the end of words
+// var name12_first;    //note: we used numbers between of words
+
+// illegal
+
+// var @name;           //note: special letters are not useds
+// var name-first;      //note: sub sign are not used
+// var 123name;         //note: we are not start with the numbers
+// var if;              //note: we not used if and if else 
+
+
+//--------------------------------------------------//
+ //  cahpter no 5 (mathematical expression 1 ) //
+  
+    //Q : NO 1 //
+
+// var num1 = 20;
+// var num2 = 6;
+
+// res = num1 % num2 ;
+
+// console.log(res);
+
+    //Q : NO 2 //
+
+// var num = 20 % 6;  //result is = 2
+
+     //Q : NO 3 //
+
+// var largeNumber = 1000;
+// document.write(largeNumber * 2000);
+
+    //Q : NO 4 //
+
+// var num1 = 40;
+// var num2 = 20;
+
+// sub = num1 - num2 ;
+// document.write(sub);
+
+    //Q : NO 5 //
+    
+    // var firstValue = 17;
+    // var secondValue = 2;  
+
+    // var remainder = firstValue % secondValue;
+    
+    // console.log(remainder);
+    
+
+    //Q : NO 6 //    
+
+// var valueOne = 10; 
+// var valueTwo = 7;
+
+// res = valueOne * valueTwo;
+
+// alert(" The value of multiplication " + valueOne + (" X ") + valueTwo + (" is = ")+ res );
+
+                    // -------------------------------the------end----------------------------------//
