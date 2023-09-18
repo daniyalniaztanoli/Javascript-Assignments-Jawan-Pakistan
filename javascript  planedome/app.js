92eda0eeee2a3d9a69f1ff41952ca941a85f1931
MUHAMMAD DANIYAL <127001145+daniyalniaztanoli@users.noreply.github.com>
2023-09-19 00:23:57 +0500

    Add files via upload

---
+++ javascript  planedome/app.js
@@ -0,0 +1,12 @@
+// var userInput = prompt("please enter plaindrome");
+
+// var reverse = '';
+
+// for(var i = userInput.length - 1; i >= 0 ; i--){
+//     reverse = reverse + userInput[i];
+// }
+// if(userInput === userInput){
+//     alert(userInput + " this is planedrome...");
+// }else{
+//     alert(userInput  + " this is not planedrome....");
+// }
